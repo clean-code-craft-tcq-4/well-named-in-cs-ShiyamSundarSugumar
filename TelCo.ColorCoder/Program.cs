2c9669839a3867cfa27c63a6611bcfc23365a0d6
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-06-14 12:32:35 +0530

    Update Program.cs

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -24,10 +24,10 @@ namespace TelCo.ColorCoder
         /// <summary>
         /// data type defined to hold the two colors of clor pair
         /// </summary>
-        internal class ColorPair
+        public class ColorPair
         {
-            internal Color majorColor;
-            internal Color minorColor;
+            public Color majorColor;
+            public Color minorColor;
             public override string ToString()
             {
                 return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
