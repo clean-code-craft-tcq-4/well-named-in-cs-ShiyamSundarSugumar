4990c88bc79f32d6a90d2fb02d38966afac00f62
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-06-14 17:02:41 +0530

    Update Main_Test_Cases.cs

--- TelCo.ColorCoder/Main_Test_Cases.cs
+++ TelCo.ColorCoder/Main_Test_Cases.cs
@@ -26,12 +26,12 @@ namespace TelCo.ColorCoder
       Debug.Assert(testPair1.majorColor == Color.Violet);
       Debug.Assert(testPair1.minorColor == Color.Green);
 
-      Program.ColorPair testPair2 = new ColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
+      Program.ColorPair testPair2 = new Program.ColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
       pairNumber = Program.GetPairNumberFromColor(testPair2);
       Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", testPair2, pairNumber);
       Debug.Assert(pairNumber == 18);
 
-      testPair2 = new ColorPair() { majorColor = Color.Red, minorColor = Color.Blue };
+      testPair2 = new Program.ColorPair() { majorColor = Color.Red, minorColor = Color.Blue };
       pairNumber = Program.GetPairNumberFromColor(testPair2);
       Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}", testPair2, pairNumber);
       Debug.Assert(pairNumber == 6);
