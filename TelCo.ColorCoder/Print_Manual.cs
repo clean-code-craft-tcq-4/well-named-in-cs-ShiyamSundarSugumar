893b7e06563e630b02ca43b0fac92cf18082ad18
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-06-14 17:09:06 +0530

    Update and rename Print_Table.cs to Print_Manual.cs

--- TelCo.ColorCoder/Print_Manual.cs
+++ TelCo.ColorCoder/Print_Manual.cs
@@ -3,15 +3,15 @@ using System.Diagnostics;
 using System.Drawing;
 namespace TelCo.ColorCoder
 {
-  internal class Print_Table
+  internal class Print_Manual
   {
-      internal void Table(Color[] colorMapMajor, Color[] colorMapMinor)
+      internal static void Table(Color[] colorMapMajor, Color[] colorMapMinor)
       {
           int MajorLength = 0, MinorLength = 0,index = 1;
           MajorLength = colorMapMajor.Length;
           MinorLength = colorMapMinor.Length;
           
-          Console.WriteLine("| Index\t | MajorColor\t\t\t  | MinorColor\t\t\t  |");
+          Console.WriteLine("| Index\t  | MajorColor\t\t\t  | MinorColor\t\t\t  |");
           Console.WriteLine("------------------------------------------------------------------------");
           for (int i = 0; i < MajorLength; i++)
           {
