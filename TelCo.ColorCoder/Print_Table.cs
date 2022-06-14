4cbe3821ea06399b1ad4b538c28941bfd8bf4e6a
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-06-14 07:44:15 +0530

    Update Print_Table.cs

--- TelCo.ColorCoder/Print_Table.cs
+++ TelCo.ColorCoder/Print_Table.cs
@@ -1,1 +1,25 @@
+using System;
+using System.Diagnostics;
+using System.Drawing;
+namespace TelCo.ColorCoder
+{
+  Class Print_Table
+  {
+        void Print_Table()
+        {
+            int MajorLength = 0, MinorLength = 0,index = 1;
+            MajorLength = colorMapMajor.Length;
+            MinorLength = colorMapMinor.Length;
+            Console.WriteLine("| Index \t  | MajorColor\t\t\t  | MinorColor\t\t\t  |");
+            for (int i = 0,j = 0; i < MajorLength; i++)
+            {
+                for (; j < MinorLength; j++)
+                {
+                    Console.WriteLine("| {0}\t  | {1} \t\t  | {2} \t\t  |", index, colorMapMajor[i], colorMapMinor[j]);
+                    index++;
+                }
+            }
+        }
+  }
 
+}
