19e7cd0c1df35a5870fe416001402b852a03e179
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-06-14 07:49:28 +0530

    Update Program.cs

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -16,11 +16,11 @@ namespace TelCo.ColorCoder
         /// <summary>
         /// Array of Major colors
         /// </summary>
-        Internal static Color[] colorMapMajor;
+        internal static Color[] colorMapMajor;
         /// <summary>
         /// Array of minor colors
         /// </summary>
-        Internal static Color[] colorMapMinor;
+        internal static Color[] colorMapMinor;
         /// <summary>
         /// data type defined to hold the two colors of clor pair
         /// </summary>
