203e79e38b5f92b23372d42a52c42b44ec5e10dd
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-06-14 17:13:22 +0530

    Update Print_Manual.cs

--- TelCo.ColorCoder/Print_Manual.cs
+++ TelCo.ColorCoder/Print_Manual.cs
@@ -11,13 +11,13 @@ namespace TelCo.ColorCoder
           MajorLength = colorMapMajor.Length;
           MinorLength = colorMapMinor.Length;
           
-          Console.WriteLine("| Index\t  | MajorColor\t\t\t  | MinorColor\t\t\t  |");
+          Console.WriteLine("|   Index\t  |   MajorColor\t\t\t  |   MinorColor\t\t\t  |");
           Console.WriteLine("------------------------------------------------------------------------");
           for (int i = 0; i < MajorLength; i++)
           {
               for (int j = 0; j < MinorLength; j++)
               {
-                  Console.WriteLine("| {0}\t  | {1} \t\t  | {2} \t\t  |", index, colorMapMajor[i], colorMapMinor[j]);
+                  Console.WriteLine("|   {0}\t  |   {1} \t\t  |   {2} \t\t  |", index, colorMapMajor[i], colorMapMinor[j]);
                   index++;
               }
           }
