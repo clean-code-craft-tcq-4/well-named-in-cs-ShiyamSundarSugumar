669d4a000cd5cdfbf9e549161e08210e9ecde55a
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-08-27 15:11:41 +0530

    Update Main_Test_Cases.cs

--- TelCo.ColorCoder/Main_Test_Cases.cs
+++ TelCo.ColorCoder/Main_Test_Cases.cs
@@ -27,25 +27,42 @@ namespace TelCo.ColorCoder
       
       pairNumber = 23;   // Color = Violet & Green
       GetColorFromPairNumberTestCase(pairNumber,Color.Violet,Color.Green);
-
+      /*
       Program.ColorPair testPair2 = new Program.ColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
       pairNumber = Get_PairNumber.GetPairNumberFromColor(testPair2);
       Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", testPair2, pairNumber);
       Debug.Assert(pairNumber == 18);
+      */
+      
+      pairNumber = 18;
+      GetPairNumberFromColorTestCase(Color.Yellow,Color.Green,pairNumber)
 
+      /*
       testPair2 = new Program.ColorPair() { majorColor = Color.Red, minorColor = Color.Blue };
       pairNumber = Get_PairNumber.GetPairNumberFromColor(testPair2);
       Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}", testPair2, pairNumber);
       Debug.Assert(pairNumber == 6);
+      */
+      pairNumber = 6;
+      GetPairNumberFromColorTestCase(Color.Red,Color.Blue,pairNumber)
 
     }
     public static void GetColorFromPairNumberTestCase(int pairNumber, Color First_Color, Color Second_Color)
     {
       Program.ColorPair testPair1 = Get_Color.GetColorFromPairNumber(pairNumber);
-      String PrintData = ("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
+      String PrintData = ("[In]Pair Number: " + pairNumber.ToString() + ",[Out] Colors: " + testPair1.ToString() + "\n");
       PrintFunction(PrintData);
       Debug.Assert(testPair1.majorColor == First_Color);
       Debug.Assert(testPair1.minorColor == Second_Color);
+    }
+    
+    public static void GetPairNumberFromColorTestCase(Color MajorColor, Color MinorColor, int PairNumber)
+    {
+      Program.ColorPair testPair2 = new Program.ColorPair() { majorColor = MajorColor, minorColor = MinorColor };
+      int pairNumber = Get_PairNumber.GetPairNumberFromColor(testPair2);
+      String PrintData =("[In]Colors:" + testPair2.ToString() + ", [Out] PairNumber: " +  pairNumber.ToString() + "\n");
+      PrintFunction(PrintData);
+      Debug.Assert(pairNumber == PairNumber);
     }
     
     public static void PrintFunction(String PrintData)
