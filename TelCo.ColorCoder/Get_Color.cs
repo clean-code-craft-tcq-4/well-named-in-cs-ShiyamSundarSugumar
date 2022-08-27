85437d173016e0a46a08221517345c44453dc8e4
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-08-27 15:29:24 +0530

    Update Get_Color.cs

--- TelCo.ColorCoder/Get_Color.cs
+++ TelCo.ColorCoder/Get_Color.cs
@@ -6,9 +6,11 @@ namespace TelCo.ColorCoder
 {
   public class Get_Color
   {
+     /// <summary>
+     /// The function supports only 1 based index. Pair numbers valid are from 1 to 25
+     /// </summary>
      internal static Program.ColorPair GetColorFromPairNumber(int pairNumber)
-        {
-            // The function supports only 1 based index. Pair numbers valid are from 1 to 25
+        {           
             int minorSize = Program.MinorColorList.Length;
             int majorSize = Program.MajorColorList.Length;
             if (pairNumber < 1 || pairNumber > minorSize * majorSize)
@@ -16,20 +18,19 @@ namespace TelCo.ColorCoder
                 throw new ArgumentOutOfRangeException(
                     string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
             }
-
-            // Find index of major and minor color from pair number
+            
             int zeroBasedPairNumber = pairNumber - 1;
             int majorIndex = zeroBasedPairNumber / minorSize;
             int minorIndex = zeroBasedPairNumber % minorSize;
 
-            // Construct the return val from the arrays
+            
             Program.ColorPair pair = new Program.ColorPair()
             {
                 majorColor = Program.MajorColorList[majorIndex],
                 minorColor = Program.MinorColorList[minorIndex]
             };
 
-            // return the value
+            
             return pair;
         }
         
