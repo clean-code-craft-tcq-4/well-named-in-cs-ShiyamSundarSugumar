0f9088606bdcc0b82c3dbffd4e3dee7e202935ab
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-08-27 15:22:18 +0530

    Update Main_Test_Cases.cs

--- TelCo.ColorCoder/Main_Test_Cases.cs
+++ TelCo.ColorCoder/Main_Test_Cases.cs
@@ -8,45 +8,22 @@ namespace TelCo.ColorCoder
   {
     public static void Test_case()
     {
-      int pairNumber = 4;
-      /*
-      Program.ColorPair testPair1 = Get_Color.GetColorFromPairNumber(pairNumber);
-      Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
-      Debug.Assert(testPair1.majorColor == Color.White);
-      Debug.Assert(testPair1.minorColor == Color.Brown);
-      */
+      int pairNumber = 4; // Color = White & Brown
       GetColorFromPairNumberTestCase(pairNumber,Color.White,Color.Brown);
       
-      pairNumber = 5;
-      /*
-      testPair1 = Get_Color.GetColorFromPairNumber(pairNumber);
-      Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
-      Debug.Assert(testPair1.majorColor == Color.White);
-      Debug.Assert(testPair1.minorColor == Color.SlateGray); */
+      pairNumber = 5;    // Color =  White & Brown
       GetColorFromPairNumberTestCase(pairNumber,Color.White,Color.SlateGray);
       
       pairNumber = 23;   // Color = Violet & Green
       GetColorFromPairNumberTestCase(pairNumber,Color.Violet,Color.Green);
-      /*
-      Program.ColorPair testPair2 = new Program.ColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
-      pairNumber = Get_PairNumber.GetPairNumberFromColor(testPair2);
-      Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", testPair2, pairNumber);
-      Debug.Assert(pairNumber == 18);
-      */
       
       pairNumber = 18;
       GetPairNumberFromColorTestCase(Color.Yellow,Color.Green,pairNumber);
 
-      /*
-      testPair2 = new Program.ColorPair() { majorColor = Color.Red, minorColor = Color.Blue };
-      pairNumber = Get_PairNumber.GetPairNumberFromColor(testPair2);
-      Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}", testPair2, pairNumber);
-      Debug.Assert(pairNumber == 6);
-      */
       pairNumber = 6;
       GetPairNumberFromColorTestCase(Color.Red,Color.Blue,pairNumber);
-
     }
+    
     public static void GetColorFromPairNumberTestCase(int pairNumber, Color First_Color, Color Second_Color)
     {
       Program.ColorPair testPair1 = Get_Color.GetColorFromPairNumber(pairNumber);
@@ -70,5 +47,4 @@ namespace TelCo.ColorCoder
       Console.WriteLine(PrintData);
     }
   }
-
 }
